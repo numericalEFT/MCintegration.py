123ea666af1fe7431b28c7030b2e4785e6ccd6d2
Tao Wang <fsxbhyy@gmail.com>
2024-10-04 00:31:56 -0400

    api refactoring wip

--- src/integrators.py
+++ src/integrators.py
@@ -13,24 +13,24 @@ class Integrator:
     def __init__(
         self,
         # bounds: Union[List[Tuple[float, float]], np.ndarray],
-        map,
+        map=None,
         neval: int = 1000,
-        batch_size: int = None,
+        nbatch: int = None,
         device="cpu",
         # device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
     ):
-        if not isinstance(map, Map):
-            map = Affine(map)
+        #if not isinstance(map, Map):
+        #    map = Affine(map)
 
         self.dim = map.dim
         self.map = map
         self.neval = neval
-        if batch_size is None:
-            self.batch_size = neval
+        if nbatch is None:
+            self.nbatch = neval
             self.neval = neval
         else:
-            self.batch_size = batch_size
-            self.neval = -(-neval // batch_size) * batch_size
+            self.nbatch = nbatch
+            self.neval = -(-neval // nbatch) * nbatch
 
         self.device = device
 
@@ -44,18 +44,18 @@ class MonteCarlo(Integrator):
         map,
         nitn: int = 10,
         neval: int = 1000,
-        batch_size: int = None,
+        nbatch: int = None,
         device="cpu",
         adapt=False,
         alpha=0.5,
     ):
-        super().__init__(map, neval, batch_size, device)
+        super().__init__(map, neval, nbatch, device)
         self.adapt = adapt
         self.alpha = alpha
         self.nitn = nitn
 
     def __call__(self, f: Callable, **kwargs):
-        u = torch.rand(self.batch_size, self.dim, device=self.device)
+        u = torch.rand(self.nbatch, self.dim, device=self.device)
         x, _ = self.map.forward(u)
         f_values = f(x)
         f_size = len(f_values) if isinstance(f_values, (list, tuple)) else 1
@@ -66,14 +66,14 @@ class MonteCarlo(Integrator):
         # var = torch.zeros((f_size, f_size), dtype=type_fval, device=self.device)
 
         result = RAvg(weighted=self.adapt)
-        epoch = self.neval // self.batch_size
+        epoch = self.neval // self.nbatch
 
         for itn in range(self.nitn):
             mean[:] = 0
             var[:] = 0
             for _ in range(epoch):
                 y = torch.rand(
-                    self.batch_size, self.dim, dtype=torch.float64, device=self.device
+                    self.nbatch, self.dim, dtype=torch.float64, device=self.device
                 )
                 x, jac = self.map.forward(y)
 
@@ -108,25 +108,25 @@ class MCMC(MonteCarlo):
         map: Map,
         nitn: int = 10,
         neval=10000,
-        batch_size=None,
+        nbatch=None,
         n_burnin=500,
         device="cpu",
         adapt=False,
         alpha=0.5,
     ):
-        super().__init__(map, nitn, neval, batch_size, device, adapt, alpha)
+        super().__init__(map, nitn, neval, nbatch, device, adapt, alpha)
         self.n_burnin = n_burnin
 
     def __call__(
         self,
         f: Callable,
-        proposal_dist="global_uniform",
+        proposal_dist="uniform",
         thinning=1,
         mix_rate=0.0,
         **kwargs,
     ):
         epsilon = 1e-16  # Small value to ensure numerical stability
-        vars_shape = (self.batch_size, self.dim)
+        vars_shape = (self.nbatch, self.dim)
         current_y = torch.rand(vars_shape, dtype=torch.float64, device=self.device)
         current_x, current_jac = self.map.forward(current_y)
         current_fval = f(current_x)
@@ -149,7 +149,7 @@ class MCMC(MonteCarlo):
         result = RAvg(weighted=self.adapt)
         result_ref = RAvg(weighted=self.adapt)
 
-        epoch = self.neval // self.batch_size
+        epoch = self.neval // self.nbatch
         n_meas = 0
         for itn in range(self.nitn):
             for i in range(epoch):
@@ -162,7 +162,7 @@ class MCMC(MonteCarlo):
                 acceptance_probs = new_weight / current_weight * new_jac / current_jac
 
                 accept = (
-                    torch.rand(self.batch_size, dtype=torch.float64, device=self.device)
+                    torch.rand(self.nbatch, dtype=torch.float64, device=self.device)
                     <= acceptance_probs
                 )
 
@@ -190,7 +190,7 @@ class MCMC(MonteCarlo):
                         )
             result.sum_neval += self.neval
             result.add(gvar.gvar(mean.item(), ((var / n_meas) ** 0.5).item()))
-            result_ref.sum_neval += self.batch_size
+            result_ref.sum_neval += self.nbatch
             result_ref.add(
                 gvar.gvar(mean_ref.item(), ((var_ref / n_meas) ** 0.5).item())
             )
@@ -204,7 +204,7 @@ class MCMC(MonteCarlo):
         if proposal_dist == "random_walk":
             step_size = kwargs.get("step_size", 0.2)
             return (u + (torch.rand_like(u) - 0.5) * step_size) % 1.0
-        elif proposal_dist == "global_uniform":
+        elif proposal_dist == "uniform":
             return torch.rand_like(u)
         # elif proposal_dist == "gaussian":
         #     mean = kwargs.get("mean", torch.zeros_like(u))
